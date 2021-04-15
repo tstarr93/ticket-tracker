1ea03554bef011636ba9b2f4234eb043647dbf79
Tim Starr <timstarr14@gmail.com>
2021-04-15 10:47:03 +0100

    Add boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import data from './data/data';
+import Card from './components/Card';
+import Counter from './components/Card';
+
+function App() {
+  return (
+    <header>
+      <h1>Ticket Tracker</h1>
+    </header>
+  );
+}
+
+export default App;
